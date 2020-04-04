622813d245a2de89513fd79501b8f567fe926fa7
Anshika Singh <43119152+anshikaX@users.noreply.github.com>
2020-04-04 17:37:17 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main1
+{
+    public static void main(String args[])
+    {
+
+        Scanner sc;
+        String s = "I love you so much. I want to marry you.";
+        sc = new Scanner(s);
+        sc.useDelimiter("[ .]+");        // we use a quantifier "+" to remove space.
+        //traversing all the tokens in the String
+        while(sc.hasNext())             //hasNext method check if there is any more token in the list and returns true if there is more token otherwise false if there is no more token left and reached the last token
+        {
+            String token = sc.next();   //next() reads and returns tokens
+            System.out.println(token);
+
+        }
+    }
+}
